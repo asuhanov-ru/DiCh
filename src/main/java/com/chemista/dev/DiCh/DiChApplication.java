49d3c0f1bbf75320ef80bf1f16f24453ced5f97d
Anton Sukhanov <suhanov_av@open.ru>
2019-07-09 17:45:17 +0300

    Starter templates for JPA

---
+++ src/main/java/com/chemista/dev/DiCh/DiChApplication.java
@@ -0,0 +1,13 @@
+package com.chemista.dev.DiCh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiChApplication.class, args);
+	}
+
+}
